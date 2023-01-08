655b5c4fc170daae9b84c328cab1b8a1c4496f0c
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2023-01-08 16:35:46 +0530

    adjusting main screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'reusableCard.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "firebase authentication",
+    title: "Seat Finder",
     debugShowCheckedModeBanner: false,
     home: new App(),
   ));
@@ -32,50 +32,53 @@ class App extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 5,
-                  color: kHeadlineColor,
+            Expanded(
+              child: Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 5,
+                    color: kHeadlineColor,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      padding: EdgeInsets.all(5.0),
-                      child: const Center(
-                        child: Text(
-                          'Find your seat here',
-                          style: TextStyle(
-                            color: kHeadlineColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        padding: EdgeInsets.all(5.0),
+                        child: const Center(
+                          child: Text(
+                            'Find your seat here',
+                            style: TextStyle(
+                              color: kHeadlineColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Find',
-                        style: TextStyle(
-                          color: kHeadlineColor,
+                    Expanded(
+                      flex: 1,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Find',
+                          style: TextStyle(
+                            color: kHeadlineColor,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            Row(
+            Expanded(
+              child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Expanded(
@@ -88,8 +91,9 @@ class App extends StatelessWidget {
                   child: ReuasbleCard(seatn: 3, seatpos: 'Upper'),
                 ),
               ],
-            )
-          ],
+              ),
+            ),
+      ],
         ),
       ),
     );
